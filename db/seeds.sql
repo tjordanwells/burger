270ec4ed7e07c487b9c2753ffc762dfbee8aa1a6
Jordan <tjordanwells@gmail.com>
2018-07-24 18:50:20 -0400

    added db /db/schema.sql /db/seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("Western BBQ Burger", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("The Classic", false);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES("California Burger", false);
