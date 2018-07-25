0550cdd7772e03e962a49ec711fc92ea49c03f46
Jordan <tjordanwells@gmail.com>
2018-07-25 11:15:08 -0400

    handled npm requirements / db connection / orm handling / working... model and controller

--- db/schema.sql
+++ db/schema.sql
@@ -7,6 +7,6 @@ USE burgers_db;
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(250) NOT NULL,
-    devoured BOOLEAN,
+    devoured BOOLEAN NOT NULL DEFAULT 0,
     PRIMARY KEY (id)
 );
