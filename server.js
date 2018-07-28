32dd7ffa32ae76b94084a1e6cfdaede39122f644
Jordan <tjordanwells@gmail.com>
2018-07-28 13:58:32 -0400

    formatting index.handlebars / adding background / adding styles

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
